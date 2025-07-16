675a8d73fccb59bb7e4cd89fe4d1c1a5fc3780b5
Zio Admin <ziobusinesses@gmail.com>
2025-07-16 11:25:22 +0530

    files visible done

--- resources/views/projects/ajax/files.blade.php
+++ resources/views/projects/ajax/files.blade.php
@@ -60,9 +60,9 @@ $deleteFilePermission = user()->permission('delete_project_files');
         <x-forms.button-primary icon="share" id="share-link" class="type-btn mb-3">
             @lang('Click Here For File Sharing')
         </x-forms.button-primary>
-        <!-- <x-forms.button-primary icon="download" id="multi-download" class="type-btn mb-3 ml-3">
+        <x-forms.button-primary icon="download" id="multi-download" class="type-btn mb-3 ml-3">
             @lang('Multiple Download')
-        </x-forms.button-primary> -->
+        </x-forms.button-primary>
 
         <div id="share-actions" style="display: none;" class="mb-3">
             <x-forms.button-primary id="share-selected" class="mr-2">
