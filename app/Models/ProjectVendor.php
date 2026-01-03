d6e0cdb3e8edc51ca0da081109a0611e213ba248
Zio Admin <ziobusinesses@gmail.com>
2026-01-03 05:43:07 +0530

    full working change notify status

--- app/Models/ProjectVendor.php
+++ app/Models/ProjectVendor.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class ProjectVendor extends BaseModel
 {
